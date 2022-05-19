d14c225260691905dfd5beaa1457f4bbeff3d4c0
AlexOlga <bulbenko@yandex.ru>
2022-05-19 08:42:08 +0300

    feat: add task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+//path.resolve(__dirname, 'dist')
+const textFile=path.join(__dirname, 'text.txt');
+const ReadStream = fs.createReadStream(textFile, 'utf-8');
+let data = '';
+ReadStream.on('data', chunk => data += chunk);
+ReadStream.on('end',()=>{console.log(data); process.exit();
+} );
+ReadStream.on('error', error => console.log('Error', error.message));
