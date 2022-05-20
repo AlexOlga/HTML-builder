42794dcd32a6bc5df40b7b694ba26c03f4db1a9e
AlexOlga <bulbenko@yandex.ru>
2022-05-20 08:16:28 +0300

    feat: add task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const destName=path.join(__dirname, '/files-copy');
+const dirName=path.join(__dirname, '/files');
+fs.mkdir( destName, {recursive:true},(err)=>{
+  if (err) {console.log(err.message);}
+});
+fs.readdir(dirName,(err, files)=>{
+  if (err) {console.log(err.message);}
+  else {
+    files.forEach(item=>{
+      const destFileName= destName+'\\'+item;
+      const fileName= dirName+'\\'+item;
+      // console.log(fileName);
+      fs.copyFile(fileName, destFileName,(err)=>{
+        if (err) {console.log(err.message);}
+        else {console.log('file copy');}
+      });    
+    });
+    //удалить 
+    fs.readdir(destName,(err,destFiles)=>{
+      destFiles.forEach(item=>{
+        if (!files.includes(item)){
+          const fileName= destName+'\\'+item;
+          fs.unlink(fileName, (err) => {
+            if (err) throw err;
+          });
+        }});      
+    });
 
+  }
+});
